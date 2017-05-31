58a6ec794479b6aac740dd2ca8579a8cdcb72573
Jordan Kay <jordanekay@mac.com>
2017-05-31 17:44:01 -0400

    Initial Commit

---
+++ Cipher/Cipher.h
@@ -0,0 +1,19 @@
+//
+//  Cipher.h
+//  Cipher
+//
+//  Created by Jordan Kay on 5/31/17.
+//  Copyright © 2017 Cultivr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cipher.
+FOUNDATION_EXPORT double CipherVersionNumber;
+
+//! Project version string for Cipher.
+FOUNDATION_EXPORT const unsigned char CipherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cipher/PublicHeader.h>
+
+
